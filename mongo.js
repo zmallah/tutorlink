7bf96d6dc102e8d29068e9a49057b88018c48880
DanteT1114 <dst1114@gmail.com>
2019-10-19 23:19:54 -0400

    connection, create, save collections and documents. User and Group Schema

--- mongo.js
+++ mongo.js
@@ -1,14 +1,10 @@
 import User from './schemas/UserSchema';
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://davidezra:tutor@tutorlinkdb-odr6c.mongodb.net/test?retryWrites=true&w=majority";
-var dbName;
-
+import {MongoClient, uri} from './Globals';
 
 MongoClient.connect(uri, function(err, db){
 	if (err) throw err;
-    
-    dbName = db.db("TLDB");
+	
+    var dbName = db.db("TLDB");
 
 	dbName.createCollection("Users", function(err, res){
 		if(err) throw err;
